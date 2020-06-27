1406a9af26d6e0c6847561b1607b0c39916ce4b0
Julius Cesar Machado Dantas <juliusmdantas@hotmail.com>
2020-06-27 18:15:37 -0300

    Flask

---
+++ app.py
@@ -0,0 +1,54 @@
+import json
+from flask import Flask, jsonify, request
+
+
+
+app = Flask(__name__)
+
+desenvolvedores = [
+    {   'id': '0',
+        'nome': 'Jose',
+        'habilidade': ['Python', 'Flask']
+     },
+
+    {   'id': '1',
+        'nome': 'Gabriel',
+        'habilidade': ['Python', 'Django', 'SQL']
+     }
+]
+
+@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido'
+            response = {'status': 'erro', 'mensagem': mensagem}
+        return jsonify(response)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status': 'sucesso',
+                        'mensagem': 'Registro excluido'})
+
+@app.route('/dev/', methods=['GET', 'POST'])
+def lista_desenvolvedores():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return jsonify(desenvolvedores[posicao])
+    elif request.method == 'GET':
+        return jsonify(desenvolvedores)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
